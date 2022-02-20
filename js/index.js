7c5e77786d807af4fac21621c879ab000761b34f
Filipi Rafael <filipirafael.123@gmail.com>
2022-02-20 01:33:06 -0300

    Finalizando stage 4

---
+++ js/index.js
@@ -0,0 +1,14 @@
+const linksSocialMedia = {
+    github: "Filipi",
+    facebook: "Filipi",
+    youtube: "Filipi",
+    Instagra: "Filipi",
+    Twitter: "Filipi"
+}
+
+function changeSocialMediaLinks() {
+
+    userName.textContent = "Marcelo"
+}
+
+changeSocialMediaLinks()
