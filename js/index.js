dedcaa42d9580bfb2853ae09dc9720fba7fa50ad
Filipi Rafael <filipirafael.123@gmail.com>
2022-02-20 21:51:27 -0300

    ✅ iFinalizando a primeira versão do website

--- js/index.js
+++ js/index.js
@@ -1,14 +1,32 @@
-const linksSocialMedia = {
-    github: "Filipi",
-    facebook: "Filipi",
-    youtube: "Filipi",
-    Instagra: "Filipi",
-    Twitter: "Filipi"
+const LinksSocialMedia = {
+    github: "filipirafael",
+    youtube: "filipirafael",
+    facebook: "filipi.rafael.180",
+    instagram: "filipi.rafael.7",
+    twitter: "filipirafael"
 }
 
-function changeSocialMediaLinks() {
+// function changeSocialMediaLinks() {
+//     for (let li of socialLinks.children) {
+//         const social = li.getAttribute('class')
 
-    userName.textContent = "Marcelo"
+//         li.children[0].href = `https://${social}.com/${LinksSocialMedia[social]}`
+//     }
+// }
+
+// changeSocialMediaLinks()
+
+function getGithubProfileInfos() {
+    const url = `https://api.github.com/users/${LinksSocialMedia.github}`
+
+    fetch(url)
+    .then(response => response.json())
+    .then(data => {
+        userName.textContent = data.name
+        userBio.textContent = data.bio
+        userLink.href = data.html_url
+        userImage.src = data.avatar_url
+    })
 }
 
-changeSocialMediaLinks()
+getGithubProfileInfos()
